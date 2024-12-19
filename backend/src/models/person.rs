0f2c045169cb39b03f0b5054cf09c8204e3eac6a
JonasHagstrom79 <carl.jonas.hagstrom@gmail.com>
2024-12-19 18:56:55 +0100

    started on person.rs for frontend

--- backend/src/models/person.rs
+++ backend/src/models/person.rs
@@ -1,15 +1,31 @@
 use serde::{Deserialize, Serialize};
-use chrono::NaiveDate;
+use chrono::NaiveDateTime;
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Person {
+    pub info: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct PersonCreate {
     pub first_name: String,
     pub last_name: String,
-    pub full_name: String,
-    pub middle_name: Option<String>,
-    pub maiden_name: Option<String>,
-    pub nick_name: Option<String>,
-    pub born: Option<String>,
-    pub deceased: Option<String>,
-    pub info: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct RelationCreate {
+    pub related_person_id: String,
+    pub relation_type: RelationType,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum RelationType {
+    FAR,
+    MOR,
+    SON,
+    DOTTER,
+}
+
+impl PersonCreate {
+
 }
